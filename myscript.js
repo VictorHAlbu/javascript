c2770c55d6c81b741c8a1be9fba7c0703ed3b9c2
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-16 09:23:40 -0300

    exercicios

---
+++ myscript.js
@@ -0,0 +1,19 @@
+// Funções sem retorno
+/*
+function dizerNome(){
+    console.log("Victor");
+}
+
+dizerNome();
+
+//fazer calculo com return /  Retornando algo
+
+function multriplicarPorDois(valor){
+    return valor * 2;
+}
+
+//console.log(multriplicarPorDois(5));
+
+let resultado = multriplicarPorDois(5);
+console.log(resultado);
+*/
---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = 'Victor';
+let idadde = 28;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
