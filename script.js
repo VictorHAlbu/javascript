82b952ba56f76467ccd24a9b66706ec6c6dd31b7
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-09 18:14:35 -0300

    Arrow Function práticando

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let estaAprovado = true;
 let sobrenome = undefined;
 let corSelecionado = null;
 */
-
+/*
 const menu = {
     calss: ".principal",
     ativar() {
@@ -14,3 +14,28 @@ const menu = {
 };
 
 menu.ativar()
+*/
+//Arrow function
+//const upperName = name => name.toUpperCase();
+
+//const upperName = name => name.toUpperCase();
+//const countLetters = word => word.length;
+
+class Menu{
+
+    constructor(menu){
+        this.menuEllement = document.querySelector(menu);
+        this.activeClass = "active";
+    }
+    addActiveEvent(){
+        this.menuEllement.addEventListener("click", (event) => {
+                event.target.classList.add(this.activeClass);
+            });
+    }
+
+}
+
+const menu = new Menu(".principal");
+menu.addActiveEvent();
+
+console.log(menu);
